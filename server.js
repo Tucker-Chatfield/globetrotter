8b0166f1386f761797b58befc6141ea37b014451
tucker-chatfield <tucker.chatfield@gmail.com>
2024-09-03 16:29:44 -0500

    debugging deployment

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-app.use(cors());
+app.use(cors({
+  origin: ['https://globetrotter-project.netlify.app'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes go here
